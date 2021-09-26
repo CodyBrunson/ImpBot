fb5cc2ada0505268221b7542a2653164f7363c9c
CodyBrunson <55360215+CodyBrunson@users.noreply.github.com>
2021-09-26 13:24:23 -0500

    Updated !xur and !x

--- ImpBot/Modules/Destiny.cs
+++ ImpBot/Modules/Destiny.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using Discord.Commands;
+using HtmlAgilityPack;
 
 namespace ImpBot.Modules
 {
@@ -14,9 +16,21 @@ namespace ImpBot.Modules
             await ReplyAsync(XurLocation());
         }
 
-        private string XurLocation()
+        private static string XurLocation()
         {
-            return "Not yet implemented.";
+            using (WebClient _WC = new())
+            {
+                String HTML = _WC.DownloadString("https://whereisxur.com/");
+                HtmlDocument Test = new();
+                Test.LoadHtml(HTML);
+                HtmlNodeCollection collection = Test.DocumentNode.SelectNodes("//h4");
+                foreach(HtmlNode title in collection)
+                {
+                    if(title.InnerText.Contains("Xûr"))
+                        return title.InnerText.ToString();
+                }
+            }
+            return "Check back later...";
         }
     }
 }
